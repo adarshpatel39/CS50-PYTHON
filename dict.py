abbfc600f5cbc852ddac0a01188498a505145929
ADARSH PATEL <adarsh2005patel@gmail.com>
2025-06-18 20:29:25 +0530

    dictionary

---
+++ dict.py
@@ -0,0 +1,16 @@
+marks={
+        "harry":100,
+        "adarsh":100,
+        "aditt":100,
+
+}
+print(marks.items())
+print(marks.keys())
+#print(marks.values(),marks.keys())
+marks.update({"harry":99})
+print(marks)
+#print(marks,type(marks))
+
+
+
+
